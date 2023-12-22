c62747c59b4cf2e47cb07b69d05bb503f66c2429
Wang HoEun <hoeunwang@gmail.com>
2023-12-23 01:38:04 +0900

    chore: eslint와 prettier 설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,35 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:react/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: [
+    "react-refresh",
+    "react",
+    "react-hooks",
+    "@typescript-eslint",
+    "prettier",
+  ],
+  // 원하는 규칙 추가하기
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    quotes: ["error", "single"],
+    "no-duplicate-imports": "error",
+    "no-console": ["warn", { allow: ["warn", "error", "info"] }],
+    "no-unused-vars": "error",
+    "no-multiple-empty-lines": "error",
   },
-}
+  settings: {
+    "import/resolver": {
+      typescript: {},
+    },
+  },
+};
