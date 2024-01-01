a83bdd08f7c25053bf794ca8c99ccc5d34d9810d
Wang HoEun <hoeunwang@gmail.com>
2024-01-01 23:37:20 +0900

    feat: router 설정

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SearchPage from './pages/SearchPage';
+import OnBoarding from './pages/OnBoarding';
 
 const Router: React.FC = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/"></Route>
+          <Route path="/" element={<OnBoarding />}></Route>
           <Route path="/search" element={<SearchPage />}></Route>
           <Route path="/:bookTitle"></Route>
         </Routes>
