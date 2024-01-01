52f74d8b94deef9d941365512c21c9a1ee2c7c54
Wang HoEun <hoeunwang@gmail.com>
2024-01-01 23:36:47 +0900

    feat: onboarding 페이지 생성

---
+++ src/pages/OnBoarding/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as S from './OnBoarding.style';
+
+const OnBoarding = () => {
+  return (
+    <S.Container>
+      <p>d</p>
+    </S.Container>
+  );
+};
+
+export default OnBoarding;
